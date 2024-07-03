3b57a0211cbea48d9f1b3fbb0d52d8e471b74f6f
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-07-03 19:05:16 +0500

    idk 5️⃣

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,10 +3,10 @@ package com.epam.utils;
 import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
-    public static boolean isPositiveNumber(String str) {
+	public static boolean isPositiveNumber(String str) {
 		if (NumberUtils.isCreatable(str)) {
 			return NumberUtils.createDouble(str) > 0;
 		}
 		return false;
-    }
+	}
 }
