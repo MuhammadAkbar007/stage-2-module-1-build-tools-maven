6807d229ac5b42fa325fe3c5213f212e33465951
MuhammadAkbar007 <fotismenos007@mail.ru>
2024-07-03 18:26:09 +0500

    another logic 🥉

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,12 +4,8 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-		if (StringUtils.isNumeric(str)) {
-			try {
-				return Integer.parseInt(str) > 0;
-			} catch (NumberFormatException e) {
-				return false;
-			}
+		if (NumberUtils.isCreatable(str)) {
+			return NumberUtils.createDouble(str) > 0;
 		}
 		return false;
     }
